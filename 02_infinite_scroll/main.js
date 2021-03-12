0d092b1a4a01ecaf6a97db88d099659fa7e7f30e
Chris <christina.friede5@gmail.com>
2021-03-12 17:49:41 +0100

    Review the code and make it shorter --> GRY code

--- 02_infinite_scroll/main.js
+++ 02_infinite_scroll/main.js
@@ -10,19 +10,35 @@ const count = 10;
 const apiKey = 'nT_rkAZljGLRk7iGaPAO27CZGdPSCqXn-xWa0UX2Uoo';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 // Create elements for Links & Photos, Add to DOM
 function displayPhotos() {
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
         // Create <img> for a photo
         const image = document.createElement('img');
-        image.setAttribute('src', photo.urls.regular);
-        image.setAttribute('alt', photo.alt_description);
-        image.setAttribute('title', photo.alt_description);
+        // image.setAttribute('src', photo.urls.regular);
+        // image.setAttribute('alt', photo.alt_description);
+        // image.setAttribute('title', photo.alt_description);
+        setAttributes(image, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
         // Put <img> inside <a>, then both inside imageContainer element
         item.appendChild(image);
         imageContainer.appendChild(item);
